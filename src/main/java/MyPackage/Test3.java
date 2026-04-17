8ef2d9cfcc862e8de9073f1cd6af309af003ce8c
Shrirang05 <123294158+Shrirang05@users.noreply.github.com>
2026-04-17 15:26:05 +0530

    Update Test3.java

--- src/main/java/MyPackage/Test3.java
+++ src/main/java/MyPackage/Test3.java
@@ -6,9 +6,9 @@ public class Test3 {
 		
 		System.out.println("Removing this statement");
 
-       System.out.println("From Git Hub");
+    
        
-       System.out.println("Test 3");
+       System.out.println("Test 3 Git Hub");
 		
 	}
 
