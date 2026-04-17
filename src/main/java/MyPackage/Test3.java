ce09c729faa52c704b68a1894f1f9d65625759d9
Shrirang <shrirangladda98@gmail.com>
2026-04-17 15:44:46 +0530

    commit on 17-04-2026

--- src/main/java/MyPackage/Test3.java
+++ src/main/java/MyPackage/Test3.java
@@ -4,9 +4,7 @@ public class Test3 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Removing this statement");
-
-       System.out.println("From Git Hub");
+		String s = "test";
 		
 	}
 
